6eebbd76c6bc1401a7d6c31e681434b0a67d2b8b
prtvpff7kf-afk <prtvpff7kf@privaterelay.appleid.com>
2026-02-13 11:24:02 +0000

    NumbersInSpanish completed with if - else if - else

---
+++ NumbersInSpanish.java
@@ -0,0 +1,24 @@
+public class NumbersInSpanish {
+    public static void main(String[] args) {
+       // read the first argument passed to the program
+       int number = Integer.parseInt(args[0]); 
+ 
+       // delete the line below and replace with your code
+       System.out.println(number);
+       
+       if (number == 1) {
+        System.out.println("uno");
+        } else if (number == 2) {
+        System.out.println("dos");
+        } else if (number == 3) {
+        System.out.println("tres");
+        } else if (number == 4) {
+        System.out.println("cuatro");
+        } else if (number == 5) {
+        System.out.println("cinco");
+        } else {
+       // handle case that number was not in range 1-5
+       System.out.println("Sorry I do not know that!");
+       }
+    }
+ }
